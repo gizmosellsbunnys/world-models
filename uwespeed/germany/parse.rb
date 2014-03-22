db8ad948845c34d98694d119d1b4fb86abfd10ed
tonywob <tony.wroblewski@gmail.com>
2014-03-22 20:59:02 +0100

    - Labelled and added Uwespeed's German buildings into seperate CSV.

---
+++ uwespeed/germany/parse.rb
@@ -0,0 +1,56 @@
+$csv = "width,length,type,region,model,height-min,height-max,stories\n"
+
+def read_entry(entry)
+  puts entry
+  file = File.open(entry, "rb")
+  contents = file.read
+  file.close
+  lines = contents.split("\n");
+  
+  x1 = x2 = y1 = y2 = 0
+  commercial = false
+  lines.each do |line|
+    if line.include?("commercialTexture")
+      commercial = true
+    end
+    if line.include?("VT ")
+      points = line.gsub(/\s+/m, ' ').strip.split(" ")
+      x = points[1].to_f
+      y = points[2].to_f
+      z = points[3].to_f
+      if x < x1
+        x1 = x
+      end
+      if x > x2
+        x2 = x
+      end
+      if z < y1
+        y1 = z
+      end
+      if z > y2
+        y2 = z
+      end
+    end
+  end
+  name = (commercial ? "COM_" : "RES_") << "#{x2-x1}x#{y2-y1}"; 
+  
+  count = 0;
+  while File.exist?(name << "_#{count}.obj")
+    count = count + 1
+  end
+  name = name << "_#{count}.obj"
+  puts name
+  $csv = "#{$csv}#{x2-x1},#{y2-y1},residential,DE,objects/uwespeed/germany/#{name},,,2\n"
+  File.rename(entry, name)
+    
+end
+
+
+entries = Dir.entries(".")
+entries.each do |entry|
+  if entry.include? ".obj"
+    read_entry(entry)
+  end
+end
+
+File.open("residential_generic.csv", 'w') {|f| f.write($csv) }
